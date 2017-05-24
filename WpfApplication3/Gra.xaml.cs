597789754239b03d8819a644e95c5b4c65084a52
patry <patry@DESKTOP-N1VE0TJ>
2017-05-24 20:25:58 +0200

    Dodaj pliki projektów.

---
+++ WpfApplication3/Gra.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Gra.xaml
+    /// </summary>
+    public partial class Gra : Window
+    {
+        int poziom;
+        public string[] w;
+        public Gra(int p)
+        {
+            poziom = p;
+            InitializeComponent();
+             w = new string[poziom];
+        }
+
+        private void btnStartKolory_Click(object sender, RoutedEventArgs e)
+        {
+            btnClose.IsEnabled = false;
+            int j = 0;
+            Random h = new Random();
+            btnStartKolory.IsEnabled = false;
+            for (int i = 0; i < poziom; i++)
+            {
+                txtI.Text = i.ToString();
+                j = h.Next(1, 4);
+                if (j == 1)
+                {
+                    w[i] = "red";
+                    var uri = new Uri("pack://application:,,,/Images/red.png");
+                    var bitmap = new BitmapImage(uri);
+                    image.Source = bitmap;
+                }
+                else if (j == 2)
+                {
+                    w[i] = "green";
+                    var uri = new Uri("pack://application:,,,/Images/green.png");
+                    var bitmap = new BitmapImage(uri);
+                    image.Source = bitmap;
+                }
+                else if (j == 3)
+                {
+                    w[i] = "yellow";
+                    var uri = new Uri("pack://application:,,,/Images/yellow.png");
+                    var bitmap = new BitmapImage(uri);
+                    image.Source = bitmap;
+                }
+                else
+                {
+                    w[i] = "blue";
+                    var uri = new Uri("pack://application:,,,/Images/blue.png");
+                    var bitmap = new BitmapImage(uri);
+                    image.Source = bitmap;
+                }
+                MessageBox.Show("Next");
+            }
+            btnClose.IsEnabled = true;
+        }
+
+        private void Zamknij_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ WpfApplication3/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int poziom = 0;
+        string[] wyniki;
+        string[] w;
+        int i;
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtWynik.IsEnabled = false;
+            btnSprawdz.IsEnabled = false;
+        }
+        public void Restart()
+        {
+            i = 0;
+            poziom = 0;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            i = 0;
+            btnSprawdz.IsEnabled = false;
+            btnStart.IsEnabled = false;
+            wyniki = new string[poziom + 1];
+            txtWynik.Text = "Poziom: " + poziom.ToString();
+            Gra game = new Gra(poziom + 1);
+            game.Show();
+            poziom++;
+            btnSprawdz.IsEnabled = true;
+            w = game.w;
+            txtTablica.Text = "";
+        }
+
+        private void btnRed_Click(object sender, RoutedEventArgs e)
+        {
+            wyniki[i] = "red";
+            txtTablica.Text += "red ";
+            i++;
+        }
+
+        private void btnBlue_Click(object sender, RoutedEventArgs e)
+        {
+            wyniki[i] = "blue";
+            txtTablica.Text += "blue ";
+            i++;
+        }
+
+        private void btnGreen_Click(object sender, RoutedEventArgs e)
+        {
+            wyniki[i] = "green";
+            txtTablica.Text += "green ";
+            i++;
+        }
+
+        private void btnYellow_Click(object sender, RoutedEventArgs e)
+        {
+            wyniki[i] = "yellow";
+            txtTablica.Text += "yellow ";
+            i++;
+        }
+
+        private void btnSprawdz_Click(object sender, RoutedEventArgs e)
+        {
+            bool wynik = false;
+            for (i = 0; i < w.Length; i++)
+            {
+                if (wyniki[i] == w[i])
+                {
+                    wynik = true;
+                    continue;
+                }
+                else
+                {
+                    MessageBox.Show("Przegrałeś na poziomie: " + poziom.ToString() + " Wpisałeś: " + wyniki[i] + " ,a powinieneś: " + w[i] + " :(\nNastąpi restart gry od początku");
+                    wynik = false;
+                    break;
+                }
+            }
+            if (wynik == true) MessageBox.Show("Przeszedłeś poziom: " + poziom.ToString() + " :)");
+            if (wynik == false) Restart();
+            btnStart.IsEnabled = true;
+            btnSprawdz.IsEnabled = false;
+        }
+    }
+}
